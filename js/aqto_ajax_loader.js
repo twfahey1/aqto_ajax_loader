14c6f42d499bc740ceb735973778da6e904e72d5
twfahey1 <twfahey1@gmail.com>
2024-06-05 12:11:57 -0500

    Revise ajax loader default style

--- js/aqto_ajax_loader.js
+++ js/aqto_ajax_loader.js
@@ -8,7 +8,7 @@
   // Add a trigger when beforeSend fires.
   Drupal.Ajax.prototype.beforeSend = function (xmlhttprequest, options) {
     console.log("beforeSend");
-    // Only apply our override on specific fields.
+    // Only apply our override on specific fields. Leaving for reference.
     // if (this.element.name == "field_name") {
     if (true) { // Always apply our override for now.
       // Copied and modified from Drupal.ajax.prototype.beforeSend in ajax.js
@@ -18,17 +18,18 @@
         '<div class="aqto-ajax-loader flex flex-col items-center space-y-2">' +
           '<div class="throbber-container">' +
             '<div class="throbber flex flex-col items-center space-y-2">' +
-              '<div class="w-6 h-6 bg-blue-500"></div>' +
-              '<div class="w-6 h-6 bg-blue-600"></div>' +
-              '<div class="w-6 h-6 bg-blue-700"></div>' +
+              '<div class="w-2 h-2 bg-blue-500"></div>' +
+              '<div class="w-2 h-2 bg-blue-600"></div>' +
+              '<div class="w-2 h-2 bg-blue-700"></div>' +
             '</div>' +
           '</div>' +
-          '<div class="message">Doing our thing...</div>' +
+          '<div class="message">Loading<span class="dots-container"></span></div>' +
         '</div>'
       );
       // Add animeJS animations to the throbber.
       var throbber = this.progress.element.find(".throbber div").toArray();
       var throbberContainer = this.progress.element.find(".throbber-container")[0];
+      var dotsContainer = this.progress.element.find(".dots-container")[0];
 
       // Create an anime.js timeline for the squares
       anime.timeline({ loop: true })
@@ -55,6 +56,32 @@
         easing: 'linear',
         loop: true
       });
+
+      // Create dots for animation
+      for (let i = 0; i < 3; i++) {
+        var dot = document.createElement('span');
+        dot.innerHTML = '.';
+        dot.classList.add('dot', 'inline-block');
+        dotsContainer.appendChild(dot);
+      }
+      
+      var dots = dotsContainer.querySelectorAll('.dot');
+
+      // Animate the dots with anime.js
+      anime.timeline({ loop: true })
+        .add({
+          targets: dots,
+          translateX: [
+            { value: 10, duration: 500 },
+            { value: 0, duration: 500 }
+          ],
+          opacity: [
+            { value: 1, duration: 500 },
+            { value: 0.2, duration: 500 }
+          ],
+          easing: 'easeInOutSine',
+          delay: anime.stagger(250, {start: 0})
+        });
 
       // Change the position of the throbber.
       $(this.element).parent().parent().after(this.progress.element);
